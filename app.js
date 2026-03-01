c9e885acb338d2bab2c793cfa9703dc5301e42b4
Tom Turner <turnertom0@gmail.com>
2026-03-01 14:34:21 -0500

    some server config stuff

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const cors = require('cors');
 const sequelize = require('./sequelize');
 const { Sequelize } = require('sequelize');
 const { requireAuth, handleGoogleAuth } = require('./Auth');
 
 allowedOrigins = [
-  'http://localhost:5173',
+  'http://localhost:3000',
   'https://song-project.xyz'
 ];
 
